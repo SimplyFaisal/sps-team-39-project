75e85a596f3ba25c8378d077c7287605a907ff37
Francisco Mendoza <33354499+frmdz@users.noreply.github.com>
2021-03-29 23:55:20 +0000

    Correct indentation

--- portfolio/src/main/java/com/google/sps/Dao.java
+++ portfolio/src/main/java/com/google/sps/Dao.java
@@ -2,11 +2,11 @@ package com.google.sps;
 
 /** DAO Interface */
 public interface Dao<T> {
-    public void create(T obj);
+  public void create(T obj);
 
-    public void update(T obj);
+  public void update(T obj);
 
-    public T read(String id);
+  public T read(String id);
 
-    public void delete(String id);
+  public void delete(String id);
 }
