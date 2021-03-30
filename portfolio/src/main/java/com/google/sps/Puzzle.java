e450ae214e85ddad12357dd69a59bf244f322afa
Francisco Mendoza <33354499+frmdz@users.noreply.github.com>
2021-03-30 00:00:56 +0000

    Fix indentation

--- portfolio/src/main/java/com/google/sps/Puzzle.java
+++ portfolio/src/main/java/com/google/sps/Puzzle.java
@@ -3,24 +3,24 @@ package com.google.sps;
 /** Puzzle domain object */
 public class Puzzle {
 
-    private String puzzleId; //Datastore generated id
-    private String image; // Blobstore url
+  private String puzzleId; //Datastore generated id
+  private String image; // Blobstore url
 
-    public String getPuzzleId() {
-      return this.puzzleId;
-    }
+  public String getPuzzleId() {
+    return this.puzzleId;
+  }
 
-    public Puzzle setPuzzleId(String puzzleId) {
-      this.puzzleId = puzzleId;
-      return this;
-    }
+  public Puzzle setPuzzleId(String puzzleId) {
+    this.puzzleId = puzzleId;
+    return this;
+  }
 
-    public String getImage() {
-      return this.image;
-    }
+  public String getImage() {
+    return this.image;
+  }
 
-    public Puzzle setImage(String image) {
-      this.image = image;
-      return this;
-    }
+  public Puzzle setImage(String image) {
+    this.image = image;
+    return this;
+  }
 }
