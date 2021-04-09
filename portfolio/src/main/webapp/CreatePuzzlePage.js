f25696991c88ccdefdea85bc9cd4a9cc4e77410c
Mark Calvario <mac2492@columbia.edu>
2021-04-09 06:28:37 +0000

    - add a try/catch to the getPuzzleLink function when it's trying to get the link of the created puzzle - change the action of the form to '/upload'

--- portfolio/src/main/webapp/CreatePuzzlePage.js
+++ portfolio/src/main/webapp/CreatePuzzlePage.js
@@ -1,27 +1,47 @@
 const createPuzzleForm = document.getElementById("create-puzzle-form");
-const difficultyInput = document.getElementById("puzzle-difficulty");
-const fileInput = document.getElementById("file-input");
-const successMessage = document.getElementById("upload-success-message");
-successMessage.classList.add("d-none")
+const responseMessage = document.getElementById("upload-message");
 
 //Handles the Create Puzzle Form Submission
 const handleCreatePuzzleFormSubmission = (e) =>{
     e.preventDefault();
-    console.log(fileInput.value);
-    console.log(difficultyInput.value);
     createPuzzleForm.reset();
+
+    //Getting the link to the newly created puzzle
     getPuzzleLink();
 }
+
 async function getPuzzleLink () {
-    const response = await fetch("/upload");
-    const text = await response.json();
-    //Creates a success message with the link to the new puzzle and displays it to the client
-    const link = document.createElement("a");
-    successMessage.innerText = "Puzzle successfully created! Share the image with friends!";
-    link.href = text;
-    link.innerText = `${text}`;
-    successMessage.append(link);
-    successMessage.classList.remove("d-none");
+    try{
+        const response = await fetch("/upload");
+        const text = await response.json();
+
+        //constructing the success message
+        const message = document.createElement("p");
+        message.innerText = "Puzzle successfully created! Share this link to your puzzle with friends!";
+        message.classList.add("text-success","my-0");
+
+        //constructing the link to the puzzle
+        const link = document.createElement("a");
+        link.href = text;
+        link.innerText = `${text}`;
+        link.classList.add("d-block", "mx-auto","text-center","pb-2");
+
+        //Add both message and link nodes as children of the 'upload-message' div 
+        responseMessage.append(link);
+        responseMessage.append(message);
+        responseMessage.classList.remove("d-none");
+    }
+    catch(error){
+        //constructing the error message
+        const message = document.createElement("p");
+        message.innerText =  "There was an error in creating a puzzle for your image. Please try again!";
+        message.classList.add("text-danger","my-0");
+
+        //Add the message node as a child of the 'upload-message' div
+        responseMessage.append(message);
+        responseMessage.classList.remove("d-none");
+
+    }
 }
 //Event Listeners for the Create Puzzle Form
 createPuzzleForm.addEventListener("submit", (e)=>{
